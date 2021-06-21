ee6ef45727bd4010b82781aafa6fa9fa8d4696c1
PssbleTrngle <nik@somethingcatchy.net>
2021-06-21 22:16:26 +0200

    update endpoint

--- pages/api/pack/index.ts
+++ pages/api/pack/index.ts
@@ -1,6 +1,8 @@
 import Joi from 'joi'
 import { ApiError } from 'next/dist/next-server/server/api-utils'
 import Pack from '../../../database/models/Pack'
+import { getMods } from '../../../lib/curseforge'
+import { getPack } from '../../../lib/token'
 import validate from '../../../lib/validate'
 
 const handler = validate(
@@ -11,13 +13,25 @@ const handler = validate(
    },
    async (req, res, session) => {
 
-      if (!session.user) throw new ApiError(403, 'Pack creation requires user')
-
       if (req.method === 'POST') {
+
+         if (!session.user) throw new ApiError(403, 'Pack creation requires user')
 
          const pack = await Pack.create({ ...req.body, author: session.user.email })
 
          return res.json(pack)
+      }
+
+      if (req.method === 'PUT') {
+
+         const { id } = await getPack(session)
+
+         const { name, author, description, links } = req.body
+         const mods = await getMods(req.body)
+
+         await Pack.findByIdAndUpdate(id, { name, mods, author, description, links })
+
+         return res.status(204).end()
       }
 
       res.status(400).send({ error: 'Invalid method' })
